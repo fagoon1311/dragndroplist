9906dcf3ea9ef6e6ab9acdf5f8fc940b11d544ff
Fagoon Sharma <fs14smailbox65@gmail.com>
2024-07-23 10:26:00 +0000

    setup for draglist

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-col">
+      <h1 className='text-2xl font-bold bg-yellow-100 text-center p-5'>Drag Drop Lists</h1>
+      <div className='flex flex-row justify-evenly'>
+        <div className='flex flex-col bg-red-400 m-10  rounded-lg h-[20rem] w-[20rem]'>
+          <h1 className='font-bold text-2xl text-center'>In progress</h1>
+        </div>
+        <div className='flex flex-col bg-green-400 m-10  rounded-lg h-[20rem] w-[20rem]'>
+          <h1 className='font-bold text-2xl text-center'>Completed</h1>
+        </div>
+        
+      </div>
     </div>
   );
 }
